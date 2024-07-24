aeb2f33abb691abf2c03c2d9b0b8afbaf01367c6
Gary Lomberg <g.lmbrg@gmail.com>
2024-07-24 15:05:13 +0200

    Add misc window with check command

--- tmux_dj_start.sh
+++ tmux_dj_start.sh
@@ -7,6 +7,7 @@ project_name=$1
 session_name="$1_session"
 run_window="run_window"
 aux_window="aux_window"
+misc_window="misc_window"
 
 # Start a new tmux session in detached mode
 tmux new-session -d -s "$session_name" -n "$run_window"
@@ -30,6 +31,11 @@ tmux split-window -h
 tmux send-keys -t "$session_name:$aux_window.0" "source tw_start base_camp" C-m
 
 tmux send-keys -t "$session_name:$aux_window.1" "source dj_manage $project_name qcluster" C-m
+
+# Create window for misc
+tmux new-window -t "$session_name" -n "$mic_window"
+
+tmux send-keys -t "$session_name:$misc_window.0" "source dj_manage $project_name check" C-m
 
 # # Attach to the session
 tmux attach -t "$session_name:$run_window.0"
